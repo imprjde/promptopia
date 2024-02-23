f98377e3ded84bd4ae5341b473ea31f9b40a9428
Prajwal <imprjde7@gmail.com>
2024-02-23 21:18:16 +0530

    first commit

---
+++ components/Loader.jsx
@@ -0,0 +1,7 @@
+export const Loader = (
+  <div className="flex space-x-1 m-auto items-center ">
+    <div className="h-3 w-3 my-1 bg-white rounded-full animate-bounce [animation-delay:-0.3s]"></div>
+    <div className="h-3 w-3 my-1 bg-white rounded-full animate-bounce [animation-delay:-0.23s]"></div>
+    <div className="h-3 w-3 my-1 bg-white rounded-full animate-bounce"></div>
+  </div>
+);
